ae57bc871d89ac672c7e724a72845ae8a4a4227b
jabirfth <elftouhjabir@gmail.com>
2022-04-07 11:16:37 +0200

    TP2.2 read write variable before declaration

---
+++ typescript/TP2_variable_declarations/variables/read-write-variable.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+console.log(num2);
+var num2 = 10;
+console.log(num1);
+var num1 = 10;
---
+++ typescript/TP2_variable_declarations/variables/read-write-variable.ts
@@ -0,0 +1,6 @@
+export {}
+console.log(num2);
+var num2:number = 10;
+
+console.log(num1);
+let num1:number = 10;
