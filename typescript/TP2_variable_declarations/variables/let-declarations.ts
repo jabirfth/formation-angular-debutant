f6c821969767a8d6e042bad6fbf1ebdf8d8422ac
jabirfth <elftouhjabir@gmail.com>
2022-04-07 11:15:32 +0200

    TP2.1 variable scopes

---
+++ typescript/TP2_variable_declarations/variables/let-declarations.ts
@@ -0,0 +1,25 @@
+export {};
+let num1:number = 1; 
+    
+function letDeclaration() { 
+    let num2:number = 2; 
+
+    if (num2 > num1) { 
+        let num3: number = 3;
+        num3++; 
+    } 
+
+    while(num1 < num2) { 
+        let num4: number = 4;
+        num1++;
+    }
+
+    console.log(num1);
+    console.log(num2); 
+    console.log(num3);
+    console.log(num4); 
+}
+
+letDeclaration();
+
+// block scope
